50b639f1bf7c6d670e4b4095a5bbd79f57987d24
Pascal Hartig <phartig@rdrei.net>
2020-11-13 18:49:32 +0000

    Document and test custom ref option

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -9,6 +9,7 @@ beforeEach(() => {
   // otherwise an environment variable set in a test can creep into a later test
   process.env['INPUT_VERSION_REGEX'] = '([0-9]+\\.[0-9]+\\.[0-9]+)';
   process.env['INPUT_VERSION_TAG_PREFIX'] = '';
+  process.env['INPUT_REF'] = '';
   process.env['INPUT_VERSION_ASSERTION_COMMAND'] = '';
   process.env['INPUT_ANNOTATED'] = 'false';
   process.env['INPUT_DRY_RUN'] = 'false';
@@ -97,6 +98,28 @@ describe('action', () => {
     expect(stdout_write).toHaveBeenCalledWith(expect.stringContaining('name=tag::8.9.1'));
     expect(stdout_write).toHaveBeenCalledWith(
       expect.stringContaining('name=commit::0123456789abcdef')
+    );
+  });
+
+  it('works with a custom ref', async () => {
+    process.env['INPUT_REF'] = 'deadbeef';
+    nock('https://api.github.com')
+      .get('/repos/theowner/therepo/git/commits/deadbeef')
+      .reply(200, {
+        message: 'this commit title contains a 8.9.1 version'
+      });
+    nock('https://api.github.com')
+      .post('/repos/theowner/therepo/git/refs', {ref: 'refs/tags/8.9.1', sha: 'deadbeef'})
+      .reply(201, {});
+
+    const stdout_write = jest.spyOn(process.stdout, 'write');
+
+    await run();
+
+    // Outputs should be empty
+    expect(stdout_write).toHaveBeenCalledWith(expect.stringContaining('name=tag::8.9.1'));
+    expect(stdout_write).toHaveBeenCalledWith(
+      expect.stringContaining('name=commit::deadbeef')
     );
   });
 
